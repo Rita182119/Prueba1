847d30a6baa53a566f983c6f8d3cd7fd33fb6c66
Rita <ramos.miluska.97@gmail.com>
2025-05-04 23:30:20 -0500

    Borrar codigo basura

--- src/Vistas/AltaProductos.java
+++ src/Vistas/AltaProductos.java
@@ -6,8 +6,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import Vistas.producto.Producto;
-
 import java.awt.Color;
 import javax.swing.JButton;
 import java.awt.Font;
--- src/Vistas/MenuPrincipal.java
+++ src/Vistas/MenuPrincipal.java
@@ -5,8 +5,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import Vistas.producto.Producto;
-
 import java.awt.Font;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
